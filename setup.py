1c5786fa87369a9ce7e63b38e1502e6df21e8215
Matej Plavevski <matej.plavevski@gmail.com>
2021-06-04 13:24:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.',
+        'Programming Language :: Python :: 3.9',
   ],
 )
 
